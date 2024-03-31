d65d779248cd7111d587d8fe6032f511e1014077
user <user@b212-0>
2024-04-01 06:21:14 +0700

    first commit

---
+++ nnn/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+# def page(request):
+#     return render(request, 'page.html')
+
+
+def page1(request):
+    return render(request, 'page1.html')
